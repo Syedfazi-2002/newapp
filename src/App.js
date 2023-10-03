f21c0649d92f3110c0482392594816dac2b8e0de
Syedfazil-2002 <fazilfazil4901@gmail.com>
2023-10-03 14:44:36 +0530

    hook2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import React from 'react';
 function App() {
+  var [x,setX]=React.useState()
+  var [a,setA]=React.useState(0)
+  var[b,setB]=React.useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="my-box">
+     <h1>App...</h1>
+     A:<input type="text" onChange={(ev)=>{setA(Number(ev.target.value))}}/>
     </div>
   );
 }
-
 export default App;
