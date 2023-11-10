0484777c2fa972ae788d2583fac8ec00cce17189
Syedfazil-2002 <fazilfazil4901@gmail.com>
2023-11-10 10:30:05 +0530

    commit

---
+++ src/Multiple.jsx
@@ -0,0 +1,106 @@
+import React from "react"
+function Components(){
+  return (
+    <div className="main-container">
+        <div className="first-div">
+            <div className="second-div">
+           <span  className="li-header">Group-1</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+        <div>
+            <div>
+            <span  className="li-header">Group-2</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-3</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-4</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-5</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-6</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-7</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-8</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-9</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+
+        <div>
+            <div>
+            <span  className="li-header">Group-10</span>
+           <span  className="li-items">India</span>
+           <span   className="li-items">Srilanka</span>
+           <span   className="li-items">Pakisthan</span>
+           <span   className="li-items">England</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Components;
