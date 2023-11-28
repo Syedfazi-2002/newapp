c0f1ee099ff0893308a51571fc2e3441b85019e8
Syedfazil-2002 <fazilfazil4901@gmail.com>
2023-11-28 19:25:51 +0530

    co

--- src/Cocktail.jsx
+++ src/Cocktail.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React,{useState} from "react";
 import axios from "axios";
 import Detail from "./Details";
 import { Link,Outlet } from "react-router-dom";
 function Drinks(){
-    var [data,setData]=React.useState([])
+    var [data,setData]= useState([])
     React.useEffect(()=>{
         axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
         .then((res)=>{
@@ -23,10 +23,29 @@ function Drinks(){
     console.log(data)
    return (
            <div>
-            <div className="nav-bar">
-               <center>
-               <h3> the cocktail </h3>
-               </center>
+            <div>
+            <nav class="navbar navbar-expand-lg bg-danger bg-body-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">The cocktail DB</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
             </div>
            <center>
            
